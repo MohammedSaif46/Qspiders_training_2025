64ac6b25ed4d46e071c121192cecfa2adc4c1d00
MOHAMMED SAIF B <129933868+MohammedSaif46@users.noreply.github.com>
2025-05-13 12:07:00 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,15 @@
+package qspiders;
+import java.util.Scanner;
+public class Forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		for(int i=1;i<=10;i++) {
+			if(i%2==0) {
+				System.out.println(i+":is even ");
+			}
+		}
+	}
+
+}
