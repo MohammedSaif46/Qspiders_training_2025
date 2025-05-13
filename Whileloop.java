168bbbf69046ab586ef465d97432ea31946692e5
MOHAMMED SAIF B <129933868+MohammedSaif46@users.noreply.github.com>
2025-05-13 11:14:03 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,13 @@
+package qspiders;
+import java.util.Scanner;
+public class Whileloop {
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int a=1;
+		int b=10;
+		while(a<=b) {
+			System.out.println(a);
+			a++;
+		}
+	}
+}
