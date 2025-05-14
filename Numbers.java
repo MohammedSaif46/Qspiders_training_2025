90185289db7a7de8cf634969dcb01755344eadeb
MOHAMMED SAIF B <129933868+MohammedSaif46@users.noreply.github.com>
2025-05-14 11:48:12 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,12 @@
+package qspiders;
+
+public class Numbers {
+	public static void main(String[] args){
+	int num=6;
+	for(int i=1;i<=num;i++) {
+		if(num%i==0) {
+			System.out.println("Number is divisble by:"+i);
+		}
+	}
+}
+}
