12662856897ef10f219bb6e29ada3253a0b6d23e
MOHAMMED SAIF B <129933868+MohammedSaif46@users.noreply.github.com>
2025-05-14 10:10:38 +0530

    Add files via upload

---
+++ Nestedloop.java
@@ -0,0 +1,15 @@
+package qspiders;
+
+public class Nestedloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num=3;
+for(int i=1;i<=num;i++) {
+	for(int j=1;j<=num;j++) {
+		System.out.println("java is love");
+	}		System.out.println("============");
+}
+	}
+
+}
